cc6d7e2ce0a7ba4ae6aa68ffe1c595603d4bd5b4
Matsvei Khmialnitski <geniusx1990@gmail.com>
2024-07-15 23:42:36 +0300

    init: start new project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import './style.css'
+import { RouterLink, RouterView } from 'vue-router'
+
+const isOpen = ref(false)
+
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+  <header class="header">
+    <div class="wrapper">
+      <div class="burger-menu" :class="{ open: isOpen }" @click="toggleMenu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <nav :class="{ open: isOpen }">
+        <RouterLink to="/">ГЛАВНАЯ</RouterLink>
+        <RouterLink to="/about">ИНФОРМАЦИЯ О ТЕСТЕ</RouterLink>
+        <RouterLink to="/start">ПРОЙТИ ТЕСТ</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
